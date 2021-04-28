a783ace05c8ba33b64548537982a5aaf45a8ce50
ahwgs <ah_wgs@126.com>
2021-04-28 15:33:15 +0800

    chore: update build script

--- packages/cli/src/command.ts
+++ packages/cli/src/command.ts
@@ -2,7 +2,7 @@
  * @Author: ahwgs
  * @Date: 2021-04-02 00:22:55
  * @Last Modified by: ahwgs
- * @Last Modified time: 2021-04-28 13:39:29
+ * @Last Modified time: 2021-04-28 15:23:41
  */
 
 import path from 'path'
